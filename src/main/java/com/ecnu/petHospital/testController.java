3be009147175e10c9d9a1eabf5a987e4e61aa80f
Jason <939848042@qq.com>
2021-03-26 15:19:52 +0800

    swagger test

---
+++ src/main/java/com/ecnu/petHospital/testController.java
@@ -0,0 +1,13 @@
+package com.ecnu.petHospital;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class testController {
+    @RequestMapping("/index")
+    public String index(){
+        return "index";
+    }
+}
